a8d96456e0485cdec8f27308efb54e8f5208567b
Monisankar Nath <monisankar.nath161998@gmail.com>
2022-05-04 01:41:37 +0530

    fix: changes required

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,1 @@
-export {Pen} from './svgs/Pen';
-export {AddButtonIcon} from './svgs/AddButtonIcon';
-export {AddButton} from './svgs/AddButton';
 export {Bump} from './svgs/Bump';
-
-export {AddIcon} from './animations/AddIcon';
---
+++ src/controllers/fetchWeather.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import {OPEN_WEATHER_API_KEY} from '@env';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+export const fetchWeather = async () => {};
